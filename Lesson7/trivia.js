1599f53df752e2db4be3178131e5493ace4c548c
MarianaRdz04 <marianardz2004@gmail.com>
2026-03-06 12:37:30 -0600

    Update trivia.js

--- Lesson7/trivia.js
+++ Lesson7/trivia.js
@@ -1,5 +1,74 @@
+function showDate() { //date function
+    let today = new Date();
+    let day = today.toLocaleDateString();
+    let time = today.toLocaleTimeString();
+
+    document.getElementById("date").innerHTML =
+        "Today is " + day + ". It is " + time + ".";
+}
+function greeting() { //greetinh function
+    let name = prompt("Enter your name please:");
+
+    if (!name) return;
+
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+
+    let hour = new Date().getHours();
+    let message;
+
+    switch (true) {
+        case (hour < 12):
+            message = "Good Morning";
+            break;
+        case (hour < 18):
+            message = "Good Afternoon";
+            break;
+        default:
+            message = "Good Evening";
+    }
+
+    document.getElementById("greeting").innerHTML =
+        message + " " + name + "!";
+}
 
 
+function getEmail() {  //email check function
+    let email;
+    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    do {
+        email = prompt("Enter your email:");
+
+        if (!pattern.test(email)) {
+            alert("Invalid email. Try again.");
+        }
+
+    } while (!pattern.test(email));
+
+    let parts = email.split("@");
+    let username = parts[0].toUpperCase();
+    let domain = parts[1];
+
+    document.getElementById("emailInfo").innerHTML =
+        "Username: " + username + "<br>Domain: " + domain;
+}
+
+
+function quoteOfDay() {  //quote function
+    let quotes = [
+        "See the good things.",
+        "The harder your work now, the easier it will be later.",
+        "Trust in yourself.",
+        "Believe in yourself.",
+        "Small steps are still process."
+    ];
+
+    let random = Math.floor(Math.random() * quotes.length);
+
+    document.getElementById("quote").innerHTML =
+        "Quote of the day:   " + quotes[random];
+}
+//Start trivia quizz
 var questions = [
     "What is the color of an emerald?",
     "Which country is both an island and a continent?",
@@ -68,77 +137,6 @@ function showScore() {
     document.getElementById("output").innerHTML +=
         "<h2>Your Final Score: " + totalScore + " / 9</h2>" +
         "<p>Percentage: " + percent + "%</p>";
-}
-
-function showDate() {
-    let today = new Date();
-    let day = today.toLocaleDateString();
-    let time = today.toLocaleTimeString();
-
-    document.getElementById("date").innerHTML =
-        "Today is " + day + ". It is " + time + ".";
-}
-function greeting() {
-    let name = prompt("Enter your name please:");
-
-    if (!name) return;
-
-    name = name.charAt(0).toUpperCase() + name.slice(1);
-
-    let hour = new Date().getHours();
-    let message;
-
-    switch (true) {
-        case (hour < 12):
-            message = "Good Morning";
-            break;
-        case (hour < 18):
-            message = "Good Afternoon";
-            break;
-        default:
-            message = "Good Evening";
-    }
-
-    document.getElementById("greeting").innerHTML =
-        message + " " + name + "!";
-}
-
-
-function getEmail() {
-    let email;
-    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    do {
-        email = prompt("Enter your email:");
-
-        if (!pattern.test(email)) {
-            alert("Invalid email. Try again.");
-        }
-
-    } while (!pattern.test(email));
-
-    let parts = email.split("@");
-    let username = parts[0].toUpperCase();
-    let domain = parts[1];
-
-    document.getElementById("emailInfo").innerHTML =
-        "Username: " + username + "<br>Domain: " + domain;
-}
-
-
-function quoteOfDay() {
-    let quotes = [
-        "See the good things.",
-        "The harder your work now, the easier it will be later.",
-        "Trust in yourself.",
-        "Believe in yourself.",
-        "Small steps are still process."
-    ];
-
-    let random = Math.floor(Math.random() * quotes.length);
-
-    document.getElementById("quote").innerHTML =
-        "Quote of the day:   " + quotes[random];
 }
 
 function runProgram() {
